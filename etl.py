b44e74b519596584a194689a3a230e6e7a12c6ac
Oh-Really <80786401+Oh-Really@users.noreply.github.com>
2024-08-28 13:54:08 +0100

    Update etl.py
    Added docstrings

--- etl.py
+++ etl.py
@@ -3,19 +3,37 @@ import psycopg2
 from sql_queries import copy_table_queries, insert_table_queries
 
 
-def load_staging_tables(cur, conn):
+def load_staging_tables(cur, conn) -> None:
+    """
+    Load the data from s3 buckets into staging tables.
+    Paramaters
+    ----------
+    cur: Database cursoer object
+    conn: Database connection object
+    """
+    cur: 
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
-def insert_tables(cur, conn):
+def insert_tables(cur, conn)-> None:
+    """
+    Insert data into fact and dimension tables from staging tables
+    Paramaters
+    ----------
+    cur: Database cursoer object
+    conn: Database connection object
+    """
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
 
 
-def main():
+def main():    
+    """
+    Create cursor and connection db objects, then call load_staging_tables and insert_tables
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
